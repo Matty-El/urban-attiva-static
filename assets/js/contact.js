bde5dcf0d91c87692ba285cb18a1c840e49cf4dc
Matt Leach <51094410+Matty-El@users.noreply.github.com>
2022-03-12 17:42:28 +0000

    Finalise contact form email configuration

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,7 +1,9 @@
 function sendMail(contact) {
-    emailjs.send("service_wgx3ahn", "contact", {
-        "to_name": contact.first_name.value,
-        "to_email": contact.email_address.value
+    emailjs.send("service_wgx3ahn", "contact_form", {
+        "to_first_name": contact.first_name.value,
+        "to_last_name": contact.last_name.value,
+        "to_email": contact.email_address.value,
+        "customer_comments": contact.comments.value
     })
     .then(
         function (response) {
@@ -14,3 +16,8 @@ function sendMail(contact) {
     );
     return false;
 }
+
+
+$("#contact-form").on("hidden.bs.modal", function () {
+    $(this).find("contact-form").trigger("reset");
+});
